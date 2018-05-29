36ec904a4174fd84ce70cf3f40c9a0a3992baae8
saurabh <saurabh@kuliza.com>
2018-05-29 18:06:02 +0530

    Create and List process using REST API and Using JPA, https://www.flowable.org/docs/userguide/index.html#_rest_support and https://www.flowable.org/docs/userguide/index.html#_jpa_support

--- bootstrap/src/main/java/org/flowableSpringboot/bootstrap/App.java
+++ bootstrap/src/main/java/org/flowableSpringboot/bootstrap/App.java
@@ -16,19 +16,11 @@ public class App {
     }
     
     @Bean
-    public CommandLineRunner init(final RepositoryService repositoryService,
-                                  final RuntimeService runtimeService,
-                                  final TaskService taskService) {
+    public CommandLineRunner init(final MyService myService) {
 
         return new CommandLineRunner() {
-            @Override
-            public void run(String... strings) throws Exception {
-                System.out.println("Number of process definitions : "
-                	+ repositoryService.createProcessDefinitionQuery().count());
-                System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
-                runtimeService.startProcessInstanceByKey("oneTaskProcess");
-                System.out.println("Number of tasks after process start: "
-                    + taskService.createTaskQuery().count());
+        	public void run(String... strings) throws Exception {
+            	myService.createDemoUsers();
             }
         };
     }
